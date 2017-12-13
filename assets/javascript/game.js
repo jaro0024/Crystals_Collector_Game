e5c98986e078a0c2465f5894a077c23bad3cfbf3
jaro0024 <30360520+jaro0024@users.noreply.github.com>
2017-12-13 10:41:55 -0500

    deleted unecessary code from reset functions on js file and added comments to html file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 
 // Global variables
-
 var randNum;
 var purpleNum, pinkNum, orangeNum, blueNum;
 var totalScore = 0;
@@ -21,7 +20,6 @@ function initGame() {
 }
 
 // To get values when player clicks on crystals and add them as the player clicks
-
 $("#purple").click(function () {
     totalScore += purpleNum;
     $("#totalScore").html(totalScore);
@@ -46,7 +44,7 @@ $("#blue").click(function () {
     winLose();
 });
 
-// Win / lose function 
+// Win / lose function:
 
 // If total score from player matches random number generated, win totals increment and game resets
 function winLose() {
@@ -66,24 +64,13 @@ function winLose() {
 
 // Function to reset the game
 function resetGame() {
-    randNum = Math.floor(Math.random() * 101 + 19);
-    purpleNum = Math.floor(Math.random() * 11 + 1);
-    pinkNum = Math.floor(Math.random() * 11 + 1);
-    orangeNum = Math.floor(Math.random() * 11 + 1);
-    blueNum = Math.floor(Math.random() * 11 + 1);
     totalScore = 0;
     $("#totalScore").html(totalScore);
     initGame();
 }
 
 // Function to reset game when clicking button
-
 function reset() {
-    randNum = Math.floor(Math.random() * 101 + 19);
-    purpleNum = Math.floor(Math.random() * 11 + 1);
-    pinkNum = Math.floor(Math.random() * 11 + 1);
-    orangeNum = Math.floor(Math.random() * 11 + 1);
-    blueNum = Math.floor(Math.random() * 11 + 1);
     totalScore = 0;
     wins = 0;
     losses = 0;
@@ -95,12 +82,10 @@ function reset() {
 
 // Ready function
 $(function () {
-
     initGame();
 
     $("#reset").click(function () {
         reset();
         alert("Restarting game!");
     });
-
 });
